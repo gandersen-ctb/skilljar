f8d9bcb65cc6ecffd0899d73782f08f4fec299ee
gandersen-ctb <g.andersen@changetheblock.com>
2026-05-08 13:00:19 +0200

    Update shared.py

--- shared.py
+++ shared.py
@@ -11,10 +11,17 @@ def add_role_message(messages, role, text):
   message = {"role": role, "content": text}
   messages.append(message)
 
-def chat(client, messages):
-  message = client.messages.create(
-    model = model,
-    max_tokens = max_tokens,
-    messages = messages
-  )
+def chat(client, messages, system=None, temperature=1.0, stop_sequences=[]):
+  params = {
+    "model": model,
+    "max_tokens": max_tokens,
+    "messages": messages,
+    "temperature": temperature,
+    "stop_sequences": stop_sequences
+  }
+
+  if system:
+    params["system"] = system
+
+  message = client.messages.create(**params)
   return messages.content[0].text

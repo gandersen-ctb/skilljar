ca5708dc797f97622116d6207ed1f9e2f258eff0
gandersen-ctb <g.andersen@changetheblock.com>
2026-05-08 15:12:25 +0200

    Update shared.py

--- shared.py
+++ shared.py
@@ -1,5 +1,7 @@
 from anthropic import Anthropic
 from dotenv import load_dotenv
+from statistics import mean
+
 load_dotenv()
 client = Anthropic()
 
@@ -45,17 +47,59 @@ def run_prompt(test_case):
     output = chat(messages)
     return output
 
+def grade_by_model(test_case, output):
+    eval_prompt = f"""
+    You are an expert AWS code reviewer. Your task is to evaluate the following AI-generated solution.
+
+    Original Task:
+    <task>
+    {test_case["task"]}
+    </task>
+
+    Solution to Evaluate:
+    <solution>
+    {output}
+    </solution>
+
+    Output Format
+    Provide your evaluation as a structured JSON object with the following fields, in this specific order:
+    - "strengths": An array of 1-3 key strengths
+    - "weaknesses": An array of 1-3 key areas for improvement
+    - "reasoning": A concise explanation of your overall assessment
+    - "score": A number between 1-10. To chose a score try justifying internally what the other 9 options should look like (better and worst than) and adjust so the score is fair.
+
+    Respond with JSON. Keep your response concise and direct.
+    Example response shape:
+    {{
+        "strengths": string[],
+        "weaknesses": string[],
+        "reasoning": string,
+        "score": number
+    }}
+        """
+    messages = []
+    add_role_message(messages, 'user', eval_prompt)
+    add_role_message(messages, 'assistant', "```json")
+    model = "claude-haiku-4-5"
+
+    eval_text = chat(messages, model, stop_sequences=["```"])
+    return json.loads(eval_text)
+
 def run_test_case(test_case):
     """Calls run_prompt, then grades the result"""
     output = run_prompt(test_case)
     
     # TODO - Grading
-    score = 10
-    
+    # score = 10
+    grade = grade_by_model(test_case, output)
+    score = grade['score']
+    reasoning = grade['reasoning']
+
     return {
         "output": output,
         "test_case": test_case,
-        "score": score
+        "score": score,
+        "reasoning": reasoning
     }
 
 def run_eval(dataset):
@@ -65,5 +109,8 @@ def run_eval(dataset):
     for test_case in dataset:
         result = run_test_case(test_case)
         results.append(result)
+    
+    avg_score = mean([result["score"] for result in results])
+    print(f"Average score: {avg_score}")
     
     return results
